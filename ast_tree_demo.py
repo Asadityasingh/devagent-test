4fc6ca3dd071f870cf8fc0cb30f25f63fe03dd2b
Aditya Singh <as25822@gmail.com>
2025-10-31 10:04:08 +0530

    Demo: AST tree visualization

--- ast_tree_demo.py
+++ ast_tree_demo.py
@@ -2,4 +2,4 @@ def process_payment(amount, user_id):
     query = "INSERT INTO payments VALUES ('" + user_id + "', " + str(amount) + ")"
     return db.execute(query)
 
-STRIPE_KEY = "sk_live_1234567890abcdef"
+STRIPE_KEY = "sk_live2_1234567890abcdef"
